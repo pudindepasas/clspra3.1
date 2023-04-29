bb01bf4815fb28f365cad6fa447d2148993ca04e
pudindepasas <stevenquezada@protonmail.com>
2023-04-28 22:26:05 -0700

    Eje3

---
+++ Ejercicio 3/Computadora.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ejercicio_3
+{
+    internal class Computadora
+    {
+        decimal capacidad;
+        decimal espacio;
+        bool encendido;
+
+        public Computadora(decimal capacidad, decimal espacio, bool encendido)
+        {
+            this.capacidad = capacidad;
+            this.espacio = espacio;
+            this.encendido = encendido;
+        }
+
+        public void aniadirDatos(int datos)
+        {
+            
+            if (encendido)
+            {
+                if (espacio < datos)
+                {
+                    Console.WriteLine("No hay suficiente espacio en el disco. Solo se añadirán {0} GB.", espacio);
+                    espacio = 0;
+                }
+                else
+                {
+                    espacio -= datos;
+                    Console.WriteLine("Se han añadido {0} GB al disco. Espacio disponible: {1} GB", datos, espacio);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No es posible, computadora apagada.");
+            }
+        }
+
+        public void eliminarDatos(int datos)
+        {
+            if (encendido)
+            {
+                espacio += datos;
+                if (espacio > capacidad)
+                {
+                    espacio = capacidad;
+                }
+                Console.WriteLine("Se han borrado {0} GBs del disco. Espacio disponible: {1} GBs", datos, espacio);
+            }
+            else
+            {
+                Console.WriteLine("No es posible, computadora apagada.");
+            }
+        }
+
+        public void encender()
+        {
+            if (!encendido)
+            {
+                Console.WriteLine("Bienvenido, computadora encendida.");
+                encendido = true;
+            }
+            else
+            {
+                Console.WriteLine("La computadora ya está encendida.");
+            }
+        }
+
+        public void apagar()
+        {
+            if (encendido)
+            {
+                Console.WriteLine("Computadora apagada.");
+                encendido = false;
+            }
+            else
+            {
+                Console.WriteLine("No es posible, computadora apagada.");
+            }
+        }
+    }
+}
+
---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computadora com = new Computadora(100,100,false);
+
+            //Estado computadora
+            Console.WriteLine("Desea encender la computadora? S/N");
+            string resp1 = Console.ReadLine();
+            if (resp1 == "S" || resp1 == "s") 
+            {
+                com.encender();
+            }
+            else
+            {
+                Console.WriteLine("Cancelado.");
+            }
+
+            //Añadir informacion
+            Console.WriteLine("Cuantos GB desea añadir?");
+            int datos = int.Parse(Console.ReadLine());
+            com.aniadirDatos(datos);
+
+            Console.WriteLine("Cuantos GB desea eliminar?");
+            int datos2 = int.Parse(Console.ReadLine());
+            com.eliminarDatos(datos2);
+
+            Console.WriteLine("Desea apagar la computadora?S/N");
+            string resp2= Console.ReadLine();
+            if (resp2 == "S" || resp2 == "s") 
+            {
+                com.apagar();
+            }
+            else
+            {
+                Console.WriteLine("Cancelado.");
+            }
+
+
+        }
+        
+    }
+}
