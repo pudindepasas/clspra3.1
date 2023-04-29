57c8bed68180b0467833db5abe40b00ed1bf8ce4
pudindepasas <132114794+pudindepasas@users.noreply.github.com>
2023-04-28 21:29:56 -0600

    Ej1

--- Ejercicio 1/Program.cs
+++ Ejercicio 1/Program.cs
@@ -3,23 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/*
-1. Crea una clase Vehiculo con las siguientes propiedades:
 
-– Id
-– Marca
-– Modelo
-– KM
-– Precio
-
--Año
-
-Debemos crear un constructor donde le pasemos los valores.
-
-Crea sus get y set de cada propiedad.
-
-Sobrescriba el metodo toString(). El cual muestre en pantalla la marca , el modelo el año y el precio del vehículo
-*/
 namespace ConsoleApp2
 {
     class Program
